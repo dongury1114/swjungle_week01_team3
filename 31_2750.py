74860ac1eb93645963438619799078208cb987bc
donggyu <dongury1114@gmail.com>
2022-04-03 16:54:49 +0900

    2022.04.03

--- 31_2750.py
+++ 31_2750.py
@@ -1,5 +1,4 @@
 # sol.1 - 반복문(while)
-from math import factorial
 import sys
 sys.stdin = open("input.txt", "rt")
 input = sys.stdin.readline
@@ -13,7 +12,7 @@ for i in number:
     print(i)
 
 
-#-풀어보기-
+#-풀어보기 개념 부터 학습해보자-
 #버블정렬
 
 #삽입정렬
---
+++ 36_2798.py
@@ -0,0 +1,8 @@
+# sol.1
+from itertools import combinations
+import sys
+sys.stdin = open("input.txt", "rt")
+input = sys.stdin.readline
+
+n, m= map(int,input().split())
+
