1b8824b8334b76cc599db420929bf04697a715eb
donggyu <dongury1114@gmail.com>
2022-04-06 00:24:03 +0900

    2022.04.06

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 import sys
 sys.stdin = open("input.txt", "rt")
+import math
 
-A, B  = map(int, input().split())
+def isPrime(num):
+    sieve = [False, False] + (True * (n-1))
+    primes = []
+    for i in (2, n + 1):
+        if sieve[i]:
+            primes.append(i)
+        for j in range(i * 2, n + 1, i):
+            sieve[j] = False
+    print(primes)
 
-if str(A)[::-1] >str(B)[::-1]:
-    print(str(A)[::-1])
-else:
-    print(str(B)[::-1])
+n = int(input())
+numbers = list(map(int, input().split()))
+count = 0 
+
+for n in numbers:
+    tmp = 0
+    if n > 1:
+        for i in range(2, int(n**0.5)):
+            if n % i == 0:
+                tmp += 1
+        if tmp ==0:
+            count += 1
+print(count)
